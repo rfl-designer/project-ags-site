8ce56850d0c7d9e332e54528bde1075bf20ba036
Rafael Oliveira <rfldesigner@outlook.com>
2021-11-11 09:25:09 -0300

    added package, postcss, readme and tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': 'Montserrat, Arial, sans-serif',
+    },
+    colors: {
+      
+      'cinza-strong': '#0F1114',
+      'cinza-medium': '#343A47',
+      'cinza-base': '#4F596D',
+      'cinza-active': '#4899ABD',
+      'cinza-disable': '#B3CAF7',
+      
+      'amarelo-strong': '#454100',
+      'amarelo-medium': '#8F8700',
+      'amarelo-base': '#F3E600',
+      'amarelo-active': '#FFF65C',
+      'amarelo-disable': '#FFFCC7',
+      
+      'preto-strong': '#191919',
+      'preto-medium': '#4D4D4D',
+      'preto-base': '#808080',
+      'preto-active': '#BFC6D9',
+      'preto-disable': '#EDF0F7',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
